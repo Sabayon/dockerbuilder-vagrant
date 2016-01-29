dc712fa9bc478205e9cc50b7044d6ea92258be3a
mudler <mudler@sabayon.org>
2016-01-29 21:34:46 +0100

    keeping cache, monthly will cleanup

--- nightly.sh
+++ nightly.sh
@@ -22,19 +22,19 @@ MAILGUN_FROM="${MAILGUN_FROM:-Excited User <mailgun\@$MAILGUN_DOMAIN_NAME\>}"
 
 die() { echo "$@" 1>&2 ; exit 1; }
 send_email() {
-local $SUBJECT="${1:-Report}"
-local $TEXT="${2:-Something went wrong}"
+local SUBJECT="${1:-Report}"
+local TEXT="${2:-Something went wrong}"
 
 [ -z "$MAILGUN_API_KEY" ] && die "You have to set MAILGUN for error reporting"
 [ -z "$MAILGUN_DOMAIN_NAME" ] && die "You have to set MAILGUN for error reporting"
 [ -z "$MAILGUN_FROM" ] && die "You have to set MAILGUN for error reporting"
 
 curl -s --user "api:${MAILGUN_API_KEY}" \
-    https://api.mailgun.net/v3/$MAILGUN_DOMAIN_NAME/messages \
-     -F from=$MAILGUN_FROM \
-    -F to=$EMAIL_NOTIFICATIONS \
-    -F subject=$SUBJECT \
-    -F text=$TEXT
+    https://api.mailgun.net/v3/"$MAILGUN_DOMAIN_NAME"/messages \
+     -F from="$MAILGUN_FROM" \
+    -F to="$EMAIL_NOTIFICATIONS" \
+    -F subject="$SUBJECT" \
+    -F text="$TEXT"
 
 }
 
@@ -45,6 +45,14 @@ echo "Starting the show."
 cd /vagrant/repositories 
 [ -d /vagrant/repositories/$DOCKER_GIT_REPOSITORY_NAME ] || git clone $DOCKER_GIT_REPOSITORY
 
+pushd /vagrant
+	
+	send_email "Syncing builder scripts to the git repository" "Hey, building process just started, just a friendly advice. If you won't see any message from me about an error, everything went OK"
+        git fetch --all                                                                                                                                                                                                                                                         
+        git reset --hard $VAGRANT_BRANCH
+
+popd
+
 pushd /vagrant/repositories/$DOCKER_GIT_REPOSITORY_NAME
 
 	git fetch --all
@@ -53,18 +61,12 @@ pushd /vagrant/repositories/$DOCKER_GIT_REPOSITORY_NAME
 	for i in "${DOCKER_IMAGES_DIRS[@]}"
 	do
 		pushd /vagrant/repositories/$DOCKER_GIT_REPOSITORY_NAME/$i
-			docker build --no-cache --rm -t $DOCKER_NAMESPACE/$i . || send_email "Building error" "Failed when building $DOCKER_NAMESPACE/$i"
+			docker build --rm -t $DOCKER_NAMESPACE/$i . || send_email "Building error" "Failed when building $DOCKER_NAMESPACE/$i"
 			docker push $DOCKER_NAMESPACE/$i || send_email "Pushing error" "Failed while pushing $DOCKER_NAMESPACE/$i"
 		popd
 	done
 
 popd
 
-pushd /vagrant
-
-        git fetch --all                                                                                                                                                                                                                                                         
-        git reset --hard $VAGRANT_BRANCH
-
-popd
 
 
