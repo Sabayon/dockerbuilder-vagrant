5be9a2376c39a25a46b890daee3e1b1ad78a38b0
mudler <mudler@sabayon.org>
2016-08-31 14:44:28 +0200

    add nginx

--- provision.sh
+++ provision.sh
@@ -2,13 +2,14 @@
 export ACCEPT_LICENSE=*
 
 equo up && sudo equo u
-equo i docker vixie-cron git wget curl net-analyzer/netcat6 git-lfs e2fsprogs sys-fs/xfsprogs docker-companion molecule-plugins molecule-core net-misc/rysnc
+equo i docker vixie-cron git wget curl net-analyzer/netcat6 git-lfs e2fsprogs sys-fs/xfsprogs docker-companion molecule-plugins molecule-core net-misc/rysnc www-servers/nginx
 echo -5 | equo conf update
 
 git lfs install
 
 cp -rfv /vagrant/confs/rsyncd.conf /etc/rsyncd.conf
-
+cp -rfv /vagrant/confs/nginx.conf /etc/nginx/nginx.conf
+sed -i 's:txt;:txt log;:g' /etc/nginx/mime.types
 
 systemctl enable rsyncd
 systemctl start rsyncd
@@ -22,5 +23,7 @@ systemctl enable build
 systemctl start build
 systemctl enable vixie-cron
 systemctl start vixie-cron
+systemctl enable nginx
+systemctl start nginx
 crontab /vagrant/crontab
 echo "@@@@ Do docker login if necessary."
